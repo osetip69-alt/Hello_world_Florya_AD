d5a87bd3b4cd049ba54adf462f05b1c35baf33b5
osetip69-alt <osetip69@gmail.com>
2026-03-12 14:38:02 +0300

    Add files via upload

---
+++ projects2/task_2_7/task_2-7_1.py
@@ -0,0 +1,5 @@
+sample_names = ["Образец_1", "Образец_2", "Образец_3", "Образец_4"]
+fixed_date = "8.03.2026"
+
+for name in sample_names:
+    print(f"Дата взятия: {fixed_date}, Название образца: {name}")
---
+++ projects2/task_2_7/task_2-7_3.py
@@ -0,0 +1,9 @@
+sequences = ["ATATACGCGTA", "CTTCGGNGGA"]
+
+for seq in sequences:
+    print(f"\nПоследовательность: {seq}")
+    print("Построчно:")
+    for letter in seq:
+        print(letter)
+
+print("\nЦикл выполнен")
